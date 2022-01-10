3c2ded09a56b504405c309abee3c6152844af7b7
Bart Massey <bart.massey@gmail.com>
2022-01-10 02:32:49 -0800

    got server working

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,74 @@
-use tide::Request;
+//! Web service for primes with large numbers of bits.
+//!
+//! Bart Massey 2021
+
+mod prime;
+use prime::*;
+
+// Web code taken from Tide examples.
+// Form taken from Code adapted from *Programming Rust* 2nd ed, Blandy et al 2021.
+
 use tide::prelude::*;
 
 #[derive(Debug, Deserialize)]
-struct Animal {
-    name: String,
-    legs: u8,
+struct Bits {
+    bits: u32,
 }
 
 #[async_std::main]
 async fn main() -> tide::Result<()> {
     let mut app = tide::new();
-    app.at("/orders/shoes").post(order_shoes);
+    app.at("/prime").post(prime_bits);
+    app.at("/").get(request_prime);
     app.listen("127.0.0.1:8080").await?;
     Ok(())
 }
 
-async fn order_shoes(mut req: Request<()>) -> tide::Result {
-    let Animal { name, legs } = req.body_json().await?;
-    Ok(format!("Hello, {}! I've put in an order for {} shoes", name, legs).into())
+async fn request_prime(_req: tide::Request<()>) -> tide::Result {
+    Ok(tide::Response::builder(tide::StatusCode::Ok)
+        .body(
+            r#"
+            <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+            <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en"><head>
+                <meta charset="UTF-8"/>
+                <meta name="google" content="notranslate">
+                <meta http-equiv="Content-Language" content="en">
+            </head><body>
+            <title>N-bit Prime</title>
+            <form action="/prime" method="post">
+                <label>bits:</label>
+                <input type="text" name="bits"/>
+                <button type="submit">Ok</button>
+            </form></body></html>
+            "#
+        )
+        .content_type("text/html;charset=utf-8")
+        .build()
+    )
+}
+
+async fn prime_bits(mut req: tide::Request<()>) -> tide::Result {
+    let Bits { bits } = req.body_form().await?;
+    let prime = prime(bits);
+
+    Ok(tide::Response::builder(tide::StatusCode::Ok)
+        .body(format!(
+            r#"
+            <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+            <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en"><head>
+                <meta charset="UTF-8"/>
+                <meta name="google" content="notranslate">
+                <meta http-equiv="Content-Language" content="en">
+            </head><body>
+            <title>{bits}-bit Prime</title>
+            <p>{prime:width$x}</p>
+            </body></html>
+            "#,
+            bits = bits,
+            prime = prime,
+            width = bits as usize / 4,
+        ))
+        .content_type("text/html;charset=utf-8")
+        .build()
+    )
 }
---
+++ src/prime.rs
@@ -0,0 +1,20 @@
+pub use num_bigint::BigUint;
+
+use glass_pumpkin::prime::check;
+use num_bigint::RandBigInt;
+
+/// Produce a prime in the range `2**n-1..=2**n`.
+// Strategy: try random integers in-range forced to odd
+// until one of them passes the `check()` test.
+pub fn prime(nbits: u32) -> BigUint {
+    // https://stackoverflow.com/a/56404365
+    let mut rng = rand::thread_rng();
+    let max: BigUint = BigUint::from(1u32) << nbits;
+    let min = max.clone() >> 1u32;
+    loop {
+        let p = rng.gen_biguint_range(&min, &max) | BigUint::from(1u32);
+        if check(&p) {
+            return p;
+        }
+    }
+}
